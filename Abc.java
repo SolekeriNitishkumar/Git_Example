7f5f5e206559c259aded9a71f0e4322db4ea14c9
SolekeriNitishKumar <snk2457@gmail.com>
2025-05-06 10:16:42 +0530

    chnages done in b2 branch

--- Abc.java
+++ Abc.java
@@ -5,6 +5,6 @@ public class Abc
 {
     public static void main(String args[])
     {
-        System.out.println("Abc file modified change in b1 branch");
+        System.out.println("Abc file modified change in b2 branch");
     }
 }
